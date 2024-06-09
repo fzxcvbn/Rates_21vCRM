de00e3a75c85078b8f506a87bd1437eb0e7ec34b
Diana <160869579+fzxcvbn@users.noreply.github.com>
2024-06-09 14:57:27 +0300

    итоговый вариант, кнопки не работают

---
+++ index.js
@@ -0,0 +1,5 @@
+let order_implementation_rates_21vcrm = document.querySelector('.order-implementation-rates-21vcrm')
+
+order_implementation_rates_21vcrm.addEventListener("click", function() {
+    
+})
